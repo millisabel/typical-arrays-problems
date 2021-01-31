d49764e7a226943118abf36b7b46cf85cba2e627
millisabel <38396309+millisabel@users.noreply.github.com>
2021-01-31 13:50:07 +0200

    feat: task Typical Arrays Problems completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    if(!arguments.length || !array.length){
+        return 0;
+    }
+    return Math.min(...array);
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+    if(!arguments.length || !array.length){
+        return 0;
+    }
+    return Math.max(...array);
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    if(!arguments.length || !array.length){
+        return 0;
+    }
+    const mean = array.reduce(function(prev, current){
+        return prev + current;
+    });
+    return mean / array.length;
+};
